3429f57b175e09e8e29108d5e3bda16f2e7637f2
Conor Pender <conor.pender@murex.com>
2018-05-10 15:23:06 +0100

    Fix some eslint issues

--- frontend/chat-app/src/components/App.js
+++ frontend/chat-app/src/components/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
-import '../styles/App.css';
 import axios from 'axios';
+import '../styles/App.css';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.defaultUrl = "http://localhost:9000/hello";
+    this.defaultUrl = 'http://localhost:9000/hello';
     this.state = {
-      name: "",
-      requestCount: 0
-    }
-  };
+      name: '',
+      requestCount: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApiData();
+    // this.interval = setInterval(() => this.fetchApiData(), 10 * 1000);
+  }
+
+  fetchApiData() {
+    axios.get(this.defaultUrl)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          name: response.data.content,
+          requestCount: response.data.id,
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
@@ -29,32 +44,10 @@ class App extends Component {
           <b>Default url:</b> {this.state.defaultUrl}
           <br/><br/>
           <b>Fetched data:</b> {this.state.name}, there have been {this.state.requestCount} requests made against the server.
-
         </p>
       </div>
-
     );
   }
-
-  fetchApiData() {
-    axios.get(this.defaultUrl)
-    .then(response => {
-      console.log(response.data);
-      let name = response.data.content;
-      let id = response.data.id;
-      this.setState({
-        name: name,
-        requestCount: id
-      })
-    })
-    .catch(err => console.log(err));
-  }
-
-  componentDidMount() {
-    this.fetchApiData();
-    // this.interval = setInterval(() => this.fetchApiData(), 10 * 1000);
-  }
-
 }
 
 export default App;
